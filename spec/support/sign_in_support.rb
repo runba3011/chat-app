917f27329c2fd7b5a74d752f08c4a8978e3681ba
runba3011 <runba3011@gmail.com>
2021-08-18 14:21:23 +0900

    SignInSupportモジュールの修正

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -1,8 +1,8 @@
 module SignInSupport
   def sign_in(user)
     visit root_path
-    fill_in 'Email' , with: @user.email
-    fill_in 'Password' , with: @user.password
+    fill_in 'Email' , with: user.email
+    fill_in 'Password' , with: user.password
     click_on 'Log in'
     expect(current_path).to eq(root_path)
   end
